d354fdee045a6802d41f8f3cf588b62bbc6c66e3
Thomas Berlok <tberlok@gmail.com>
2023-01-23 16:33:28 +0100

    Create run_all_examples.sh

---
+++ run_all_examples.sh
@@ -0,0 +1,30 @@
+# Simple example of loading a snapshot
+python examples/loading_data_example.py
+
+# Slicing
+python examples/slicer_example.py
+
+# Projections
+python examples/projector_example.py
+python examples/nested_projector_example.py
+
+
+# Histograms
+python examples/histogram1D_example.py
+python examples/histogram2d_example.py
+
+# Loading images
+python examples/image_reader_example.py
+
+# Creating radial profiles
+python examples/radial_profiles_example.py
+
+# Setting up user-defined functions for obtaining derived variables
+python examples/user_defined_functions_example.py
+
+# Using a configuration script
+python examples/using_a_paicos_config.py
+python examples/example_paicos_config.py
+
+# Select an index of a snapshot
+python examples/select_subset_of_snap.py
