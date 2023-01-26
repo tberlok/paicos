c4b33f6a18b7e02a6de3fc589af2cba34b3087aa
Thomas Berlok <tberlok@gmail.com>
2023-01-26 13:00:43 +0100

    Update histogram and its example

--- examples/histogram1D_example.py
+++ examples/histogram1D_example.py
@@ -3,6 +3,8 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 
+logscale = True
+
 plt.figure(1)
 plt.clf()
 fig, axes = plt.subplots(num=1, ncols=3, sharex=True)
@@ -10,9 +12,8 @@ for ii in range(2):
     snap = pa.Snapshot(pa.root_dir + "/data", 247)
     center = snap.Cat.Group["GroupPos"][0]
 
-    pos = snap["0_Coordinates"]
-
-    r = np.sqrt(np.sum((pos - center[None, :]) ** 2.0, axis=1))
+    r = np.sqrt(np.sum((snap["0_Coordinates"] - center[None, :]) ** 2.0,
+                axis=1))
 
     if pa.settings.use_units:
         r_max = 10000 * r.unit_quantity
@@ -21,10 +22,12 @@ for ii in range(2):
 
     index = r < r_max * 1.1
 
-    bins = np.linspace(0, r_max, 150)
+    snap = snap.select(index)
 
-    # Rewrite to use same syntax for bins as the 2D histogram
-    # bins = np.logspace(-2, np.log10(r_max), 1000)
+    r = np.sqrt(np.sum((snap["0_Coordinates"] - center[None, :]) ** 2.0,
+                axis=1))
+
+    bins = [1e-3*r_max, r_max*1.1, 50]
 
     B2 = np.sum((snap["0_MagneticField"]) ** 2, axis=1)
     Volume = snap["0_Volume"]
@@ -32,11 +35,11 @@ for ii in range(2):
     Temperatures = snap["0_Temperatures"]
 
     if ii == 0:
-        h_r = pa.Histogram(r[index], bins, verbose=True)
-        B2TimesVolume = h_r.hist((B2 * Volume)[index])
-        Volume = h_r.hist(Volume[index])
-        TTimesMasses = h_r.hist((Masses * Temperatures)[index])
-        Masses = h_r.hist(Masses[index])
+        h_r = pa.Histogram(r, bins, logscale=logscale, verbose=True)
+        B2TimesVolume = h_r.hist((B2 * Volume))
+        Volume = h_r.hist(Volume)
+        TTimesMasses = h_r.hist((Masses * Temperatures))
+        Masses = h_r.hist(Masses)
 
         axes[0].loglog(h_r.bin_centers, Masses / Volume)
         axes[1].loglog(h_r.bin_centers, B2TimesVolume / Volume)
@@ -48,17 +51,27 @@ for ii in range(2):
             axes[1].set_ylabel((B2TimesVolume / Volume).label("B^2"))
             axes[2].set_ylabel((TTimesMasses / Masses).label("T"))
     else:
+        bins = np.linspace(0, r_max, 150)
+        if pa.settings.use_units:
+            r_max = r_max.value
+            B2 = B2.value
+            Volume = Volume.value
+            Temperatures = Temperatures.value
+            Masses = Masses.value
+            r = r.value
+
+        bins = np.logspace(np.log10(1e-3*r_max), np.log10(r_max), 50)
         B2TimesVolume, edges = np.histogram(
-            r[index], weights=(B2 * Volume)[index], bins=bins
+            r, weights=(B2 * Volume), bins=bins
         )
 
-        Volume, edges = np.histogram(r[index], weights=Volume[index], bins=bins)
+        Volume, edges = np.histogram(r, weights=Volume, bins=bins)
 
         TTimesMasses, edges = np.histogram(
-            r[index], weights=(Masses * Temperatures)[index], bins=bins
+            r, weights=(Masses * Temperatures), bins=bins
         )
 
-        Masses, edges = np.histogram(r[index], weights=Masses[index], bins=bins)
+        Masses, edges = np.histogram(r, weights=Masses, bins=bins)
 
         bin_centers = 0.5 * (edges[1:] + edges[:-1])
 
--- paicos/histogram.py
+++ paicos/histogram.py
@@ -1,5 +1,6 @@
 import numpy as np
-from . import units as pu
+from . import util
+from . import settings
 
 
 class Histogram:
@@ -9,7 +10,7 @@ class Histogram:
 
     The heavy part of the computation is stored in idigit
     """
-    def __init__(self, x_variable, bins, verbose=False):
+    def __init__(self, x, bins, logscale=False, verbose=False):
         """
         Initialize the class.
 
@@ -21,18 +22,74 @@ class Histogram:
             verbose (bool, optional): If True, prints the time taken to
                                       digitize the x-coordinates.
         """
+
+        if isinstance(bins, int):
+            bins = [x.min(), x.max(), bins]
+
+        self.logscale = logscale
         self.verbose = verbose
+
+        self.edges, self.centers = self._make_bins(bins)
+        self.bin_centers = self.centers
+        self.bin_edges = self.edges
 
         if self.verbose:
             import time
             t = time.time()
             print('Digitize for histogram begun')
 
-        self.idigit = np.digitize(x_variable, bins)
+        self.idigit = np.digitize(x, self.edges)
         if self.verbose:
             print('This took {:1.2f} seconds'.format(time.time()-t))
-        self.bins = bins
-        self.bin_centers = 0.5*(bins[1:] + bins[:-1])
+
+    def _make_bins(self, bins):
+        """
+        Private method to calculate the edges and centers of the bins
+        given lower, upper and number of bins.
+
+        Parameters:
+            bins (tuple): Tuple of lower edge, upper edge and number of
+                          bins for the axis
+        Returns:
+            edges (array): Edges of the bins
+            centers (array): Centers of the bins
+        """
+
+        lower, upper, nbins = bins
+        edges, centers = self.__make_bins(lower, upper, nbins)
+        from . import settings
+        if settings.use_units:
+            assert lower.unit == upper.unit
+            edges = np.array(edges)*lower.unit_quantity
+            centers = np.array(centers)*lower.unit_quantity
+        return edges, centers
+
+    @util.remove_astro_units
+    def __make_bins(self, lower, upper, nbins):
+        """
+        Private method to calculate the edges and centers of the bins
+        in logscale or linear scale based on the class variable logscale
+        Parameters:
+            lower (float): Lower edge of the bin
+            upper (float): Upper edge of the bin
+            nbins (int): Number of bins for the axis
+        Returns:
+            edges (array): Edges of the bins
+            centers (array): Centers of the bins
+        """
+
+        if self.logscale:
+            lower = np.log10(lower)
+            upper = np.log10(upper)
+
+        edges = lower + np.arange(nbins+1)*(upper-lower)/nbins
+        centers = 0.5*(edges[1:] + edges[:-1])
+
+        if self.logscale:
+            edges = 10**edges
+            centers = 10**centers
+
+        return edges, centers
 
     def hist(self, weights):
         """
@@ -44,18 +101,14 @@ class Histogram:
         Returns:
             array: Histogram of the data.
         """
-        from .cython.histogram import get_hist_from_weights_and_idigit
+        from .cython.histogram import get_hist_from_weights_and_idigit as func
 
-        if hasattr(weights, 'unit'):
-            if isinstance(weights, pu.PaicosQuantity):
-                factor = weights.unit_quantity
-            else:
-                factor = 1.0*weights.unit
-            weights = weights.value
-        else:
-            factor = 1.0
+        get_hist_from_weights_and_idigit = util.remove_astro_units(func)
 
         # compute histogram using pre-digitized x-coordinates and given weights
-        f = get_hist_from_weights_and_idigit(self.bins.shape[0], weights,
-                                             self.idigit)
-        return f * factor
+        hist = get_hist_from_weights_and_idigit(self.edges.shape[0], weights,
+                                                self.idigit)
+        if settings.use_units:
+            hist = hist * weights.unit_quantity
+
+        return hist
