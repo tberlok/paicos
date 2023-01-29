5f190fd72c33617c17223b08bfbc46edbdcec324
Thomas Berlok <tberlok@gmail.com>
2023-01-29 13:42:44 +0100

    Rewriting PaicosReader (unusuable commit)

--- paicos/paicos_readers.py
+++ paicos/paicos_readers.py
@@ -1,22 +1,45 @@
-from .arepo_converter import ArepoConverter
 from . import util
 import h5py
+import os
 
 
 class PaicosReader(dict):
 
-    def __init__(self, basedir, snapnum=None, basename="projection",
-                 load_all=True):
+    def __init__(self, basedir='.', snapnum=None, basename="snap",
+                 load_all=True, to_physical=False,
+                 basesubdir='snapdir', verbose=False):
 
         if basedir[-1] != '/':
             basedir += '/'
-        #
-        self.filename = basedir + basename
 
-        if snapnum is not None:
-            self.filename += '_{:03d}.hdf5'.format(snapnum)
+        if snapnum is None:
+            self.filename = basedir + basename + '.hdf5'
+            msg = 'File: {} not found found'.format(self.filename)
+            assert os.path.exists(self.filename), msg
         else:
-            self.filename += '.hdf5'
+            single_file = basename + "_{:03d}.hdf5"
+            multi_file = basesubdir + '_{:03d}/' + basename + '_{:03d}.{}.hdf5'
+            multi_wo_dir = basename + '_{:03d}.{}.hdf5'
+            #
+
+            single_file = basedir + single_file.format(snapnum)
+            multi_file = basedir + multi_file.format(snapnum, snapnum, '{}')
+            multi_wo_dir = basedir + multi_wo_dir.format(snapnum, '{}')
+
+            if os.path.exists(single_file):
+                self.multi_file = False
+                self.filename = single_file
+            elif os.path.exists(multi_file.format(0)):
+                self.multi_file = True
+                self.first_file_name = self.filename = multi_file.format(0)
+                self.no_subdir = False
+            elif os.path.exists(multi_wo_dir.format(0)):
+                self.multi_file = True
+                self.first_file_name = self.filename = multi_wo_dir.format(0)
+                self.no_subdir = True
+            else:
+                err_msg = "File: {} not found found"
+                raise FileNotFoundError(err_msg.format(self.file_name))
 
         with h5py.File(self.filename, 'r') as f:
             self.Header = dict(f['Header'].attrs)
@@ -24,22 +47,22 @@ class PaicosReader(dict):
             self.Parameters = dict(f['Parameters'].attrs)
             keys = list(f.keys())
 
-        self.converter = ArepoConverter(self.filename)
+        # self.converter = ArepoConverter(self.filename)
 
-        self.h = self.converter.h
-        self.scale_factor = self.a = self.converter.a
+        # self.h = self.converter.h
+        # self.scale_factor = self.a = self.converter.a
 
-        if self.converter.ComovingIntegrationOn:
-            self.redshift = self.z = self.converter.z
-            self.age = self.converter.age
-            self.lookback_time = self.converter.lookback_time
-        else:
-            self.time = self.converter.time
+        # if self.converter.ComovingIntegrationOn:
+            # self.redshift = self.z = self.converter.z
+            # self.age = self.converter.age
+            # self.lookback_time = self.converter.lookback_time
+        # else:
+            # self.time = self.converter.time
 
         # Load all data sets
-        if load_all:
-            for key in keys:
-                self.load_data(key)
+        # if load_all:
+        #     for key in keys:
+        #         self.load_data(key)
 
     def load_data(self, name, group=None):
 
@@ -54,6 +77,22 @@ class PaicosReader(dict):
                     if name not in self:
                         self[name] = {}
                     self[name][data_name] = data
+
+
+class Catalog2(PaicosReader):
+    def __init__(self, basedir='.', snapnum=None, load_all=True,
+                 to_physical=False, subfind_catalog=True, verbose=False):
+
+        if subfind_catalog:
+            basename = 'fof_subhalo_tab'
+        else:
+            basename = 'fof_tab'
+
+        super().__init__(basedir=basedir, snapnum=snapnum, basename=basename,
+                         load_all=load_all, to_physical=to_physical,
+                         basesubdir='groups', verbose=verbose)
+
+
 
 
 class ImageReader(PaicosReader):
