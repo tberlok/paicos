b7ba9010262dd6e7f0cd049e1bd2c3acc33e237b
Thomas Berlok <tberlok@gmail.com>
2023-02-08 13:04:47 +0100

    Increase version number

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ with open('README.md') as f:
 
 setup(
     name='paicos',
-    version='0.1.0',
+    version='0.1.1',
     description=('An object-oriented Python package for analysis of '
                  + '(cosmological) simulations performed with Arepo.'),
     url='https://github.com/tberlok/paicos',
