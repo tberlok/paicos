6ed81378a5eb782baa8cf22bbd9965dc24c9e30b
Thomas Berlok <tberlok@gmail.com>
2023-04-15 14:40:21 +0200

    Flake8 errors

--- examples/projector_nongas_example.py
+++ examples/projector_nongas_example.py
@@ -48,10 +48,10 @@ for use_units in [False, True]:
 
         # Make a plot
         axes[0,ii].imshow(np.array(Density), origin='lower',
-                        extent=np.array(projector.extent), norm=LogNorm())
+                          extent=np.array(projector.extent), norm=LogNorm())
         axes[0, 1].set_title('SPH projector')
         axes[1,ii].imshow(np.array(Density_tree), origin='lower',
-                        extent=np.array(projector.extent), norm=LogNorm())
+                          extent=np.array(projector.extent), norm=LogNorm())
         axes[1, 1].set_title('Tree projector')
 
     plt.show()
