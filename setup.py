722114f80283f1bb4c17e8d1a44670db480fc78d
Thomas Berlok <tberlok@gmail.com>
2023-11-02 19:29:14 +0100

    Bump version and cmdclass=build_ext

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 from setuptools import Extension
 from Cython.Build import cythonize
 from Cython.Compiler import Options
+import Cython
 import sys
 import os
 import numpy
@@ -17,8 +18,7 @@ compiler_directives = {"boundscheck": False, "cdivision": True,
                        "wraparound": False, 'language_level': "3"}
 
 
-# sources = glob.glob('paicos/cython/*.pyx')
-# sources += glob.glob('paicos/cython/*.c')
+
 include_dirs = ['paicos/cython/', numpy.get_include()]
 extra_compile_args = ['-fopenmp', "-DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION",
                       "-Wno-unused-function"]
@@ -70,10 +70,9 @@ install_requires = ['scipy',
 with open('README.md') as f:
     long_description = f.read()
 
-
 setup(
     name='paicos',
-    version='0.1.4',
+    version='0.1.5',
     description=('An object-oriented Python package for analysis of '
                  + '(cosmological) simulations performed with Arepo.'),
     url='https://github.com/tberlok/paicos',
@@ -84,6 +83,9 @@ setup(
     license='BSD 3-clause',
     packages=setuptools.find_packages(),
     install_requires=install_requires,
+    package_data = {'paicos/cython': ['*.c', '*.so']},
     classifiers=['Programming Language :: Python :: 3'],
     ext_modules=cythonize(ext_modules,
-                          compiler_directives=compiler_directives))
+                          compiler_directives=compiler_directives),
+    cmdclass={'build_ext': Cython.Build.build_ext},
+    )
