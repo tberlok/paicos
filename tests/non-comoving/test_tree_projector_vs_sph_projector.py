tberlok/paicos

commit b46555f61fb19c86f2ffbd3d56f92f8a80f9f47e

Clean up of GPU and treeprojector code

paicos/image_creators/tree_projector.py
@@ -284,21 +284,17 @@ class TreeProjector(ImageCreator):
             if not isinstance(variable, np.ndarray):
                 raise RuntimeError('Unexpected type for variable')
 
-        area_per_pixel = self.area_per_pixel
-
         if additive:
-            dV = area_per_pixel * self.delta_depth
             avail_list = (list(self.snap.keys()) + self.snap._auto_list)
             if f'{parttype}_Volume' in avail_list:
-                weight = dV / self.snap[f'{parttype}_Volume'][self.index]
-                variable = variable[self.index] * weight
-                projection = np.sum(variable, axis=2) / area_per_pixel
+                variable_density = variable[self.index] / self.snap[f'{parttype}_Volume'][self.index]
+                projection = np.sum(variable_density * self.delta_depth, axis=2) / self.depth
             else:
                 err_msg = (f"The volume field for parttype {parttype} is required when"
                            + "using additive=True")
                 raise RuntimeError(err_msg)
         else:
             variable = variable[self.index]
-            projection = np.mean(variable, axis=2)
+            projection = np.sum(variable * self.delta_depth, axis=2) / self.depth
 
         return projection

paicos/__init__.py
@@ -4,7 +4,8 @@ simulations performed with Arepo. Its main feature is that it includes
 handling of units, derived variables, loading of data upon request.
 
 The module includes a way of writing and reading data with units.
-The code is parallel with an OpenMP Cython implementation.
+The code is parallel with an OpenMP Cython implementation
+and a CUDA GPU implementation for visualization.
 """
 
 __version__ = "0.1.5"
@@ -161,6 +162,13 @@ def give_openMP_warnings(option):
     settings.give_openMP_warnings = option
 
 
+def give_cuda_import_warnings(option):
+    """
+    Turns on/off cuda import warnings on startup
+    """
+    settings.give_cuda_import_warnings = option
+
+
 def set_aliases(aliases):
     """
     Assign a list of aliases for use as keys in snapshot objects.
@@ -217,14 +225,14 @@ try:
     from .image_creators.gpu_sph_projector import GpuSphProjector
     from .image_creators.gpu_ray_projector import GpuRayProjector
 except Exception as e:
-    if not settings.disable_cuda_import_warnings:
+    if settings.give_cuda_import_warnings:
         import warnings
         print(e)
         err_msg = ('\nPaicos: The simple cuda example using cupy and numba failed '
                    'with the error above. Please check the official documentation for '
                    'cupy and numba for installation procedure. Note that you need '
                    ' a cuda-enabled GPU.\n')
-    warnings.warn(err_msg)
+        warnings.warn(err_msg)
 
 # Do this at start up
 util.check_if_omp_has_issues()

paicos/image_creators/gpu_ray_projector.py
@@ -281,7 +281,7 @@ class GpuRayProjector(ImageCreator):
         projection = cp.asnumpy(image)
         return projection
 
-    def project_variable(self, variable):
+    def project_variable(self, variable, additive=False):
         """
         projects a given variable onto a 2D plane.
 
@@ -299,6 +299,10 @@ class GpuRayProjector(ImageCreator):
         # This calls _do_region_selection if resolution, Orientation,
         # widths or center changed
         self._check_if_properties_changed()
+
+        if additive:
+            err_msg = "GPU ray tracer does not yet support additive=True"
+            raise RuntimeError(err_msg)
 
         if isinstance(variable, str):
             variable_str = str(variable)
@@ -336,12 +340,11 @@ class GpuRayProjector(ImageCreator):
         assert projection.shape[0] == self.npix_height
         assert projection.shape[1] == self.npix_width
 
-        area_per_pixel = self.area / np.prod(projection.shape)
-
         if isinstance(variable, units.PaicosQuantity):
-            projection = projection * variable.unit_quantity
+            unit_length = self.snap['0_Coordinates'].uq
+            projection = projection * variable.unit_quantity * unit_length
 
-        return projection / area_per_pixel
+        return projection / self.depth
 
     def __del__(self):
         """

paicos/image_creators/gpu_sph_projector.py
@@ -418,12 +418,10 @@ class GpuSphProjector(ImageCreator):
         assert projection.shape[0] == self.npix_height
         assert projection.shape[1] == self.npix_width
 
-        area_per_pixel = self.area / np.prod(projection.shape)
-
         if isinstance(variable, units.PaicosQuantity):
             projection = projection * variable.unit_quantity
 
-        return projection / area_per_pixel
+        return projection / self.area_per_pixel
 
     def __del__(self):
         """

paicos/settings.py
@@ -20,5 +20,12 @@ use_aliases = False
 
 # Strictly enforce units
 strict_units = True
+
+# Enable spherical coordinates
+spherical_coords = False
+
+# Enable cylindrical coordinates
+cyl_coords = False
+
 # Whether to give cuda import warnings
-disable_cuda_import_warnings = False
+give_cuda_import_warnings = True

tests/cuda-gpu/test_gpu_binary_tree.py
@@ -13,7 +13,7 @@ def test_gpu_binary_tree():
 
     index = pa.util.get_index_of_cubic_region_plus_thin_layer(
         snap['0_Coordinates'], center, widths,
-        snap['0_Diameters'], snap.box)
+        2.0 * snap['0_Diameters'], snap.box)
 
     snap = snap.select(index, parttype=0)
 

commit 9f0cbc714bcbed8467c7f67d33cf0203fad23d93

Regression fix and new test
Bug in the scaling of tree_projector with additive=True introduced in
b46555f61fb19c86f2ffbd3d56f92f8a80f9f47e

Now fixed and I also introduced a new test that would have caught this regression. Thanks to @lenajlassi for pointing this out!

examples/tree_projector_example.py
@@ -21,46 +21,51 @@ M = projector.project_variable('0_Masses')
 V = projector.project_variable('0_Volume')
 rho = M / V
 
-plt.figure(1)
-plt.clf()
-fig, axes = plt.subplots(num=1, nrows=3, sharex=True)
+rel_rho = (np.abs(tree_rho - rho) / tree_rho)
+rel_M = (np.abs(tree_M - M) / tree_M)
 
-if True:
-    # Define new imshow_wo_units that automatically plots the values
-    imshow_wo_units = pa.util.remove_astro_units(axes[0].imshow)
-    imshow_wo_units(rho, extent=projector.extent,
-                    vmin=rho.min(), vmax=rho.max())
-    axes[0].set_ylabel(projector.extent.label('y'))
-    axes[0].set_title('Sph projection')
+for ii in range(1, 3):
 
-    imshow_wo_units = pa.util.remove_astro_units(axes[1].imshow)
-    imshow_wo_units(tree_rho.value, extent=tree_projector.extent.value,
-                    vmin=rho.min(), vmax=rho.max())
-    axes[1].set_ylabel(tree_projector.extent.label('y'))
-    axes[1].set_title('Tree projection')
+    plt.figure(ii)
+    plt.clf()
+    fig, axes = plt.subplots(num=ii, nrows=3, sharex=True)
 
-    axes[2].imshow(np.abs(tree_rho.value - rho.value) / tree_rho.value,
-                   extent=tree_projector.extent.value)
-    axes[2].set_ylabel(tree_projector.extent.label('y'))
-    axes[2].set_xlabel(tree_projector.extent.label('x'))
-    axes[2].set_title('Rel difference')
+    if ii == 1:
+        # Define new imshow_wo_units that automatically plots the values
+        fig.suptitle('Density plots (by dividing additive projections)')
+        imshow_wo_units = pa.util.remove_astro_units(axes[0].imshow)
+        imshow_wo_units(rho, extent=projector.extent,
+                        vmin=rho.min(), vmax=rho.max())
+        axes[0].set_ylabel(projector.extent.label('y'))
+        axes[0].set_title('Sph projection')
 
-else:
-    # Define new imshow_wo_units that automatically plots the values
-    imshow_wo_units = pa.util.remove_astro_units(axes[0].imshow)
-    imshow_wo_units(M, extent=projector.extent)
-    axes[0].set_ylabel(projector.extent.label('y'))
-    axes[0].set_title('Sph projection')
+        imshow_wo_units = pa.util.remove_astro_units(axes[1].imshow)
+        imshow_wo_units(tree_rho.value, extent=tree_projector.extent.value,
+                        vmin=rho.min(), vmax=rho.max())
+        axes[1].set_ylabel(tree_projector.extent.label('y'))
+        axes[1].set_title('Tree projection')
 
-    imshow_wo_units = pa.util.remove_astro_units(axes[1].imshow)
-    imshow_wo_units(tree_M.value, extent=tree_projector.extent.value)
-    axes[1].set_ylabel(tree_projector.extent.label('y'))
-    axes[1].set_title('Tree projection')
+        axes[2].imshow(rel_rho.value, extent=tree_projector.extent.value)
+        axes[2].set_ylabel(tree_projector.extent.label('y'))
+        axes[2].set_xlabel(tree_projector.extent.label('x'))
+        axes[2].set_title('Rel difference')
 
-    axes[2].imshow(np.abs(tree_M.value - M.value) / tree_M.value,
-                   extent=tree_projector.extent.value)
-    axes[2].set_ylabel(tree_projector.extent.label('y'))
-    axes[2].set_xlabel(tree_projector.extent.label('x'))
-    axes[2].set_title('Rel difference')
+    if ii == 2:
+        fig.suptitle('Mass plots')
+        # Define new imshow_wo_units that automatically plots the values
+        imshow_wo_units = pa.util.remove_astro_units(axes[0].imshow)
+        imshow_wo_units(M, extent=projector.extent)
+        axes[0].set_ylabel(projector.extent.label('y'))
+        axes[0].set_title('Sph projection')
+
+        imshow_wo_units = pa.util.remove_astro_units(axes[1].imshow)
+        imshow_wo_units(tree_M.value, extent=tree_projector.extent.value)
+        axes[1].set_ylabel(tree_projector.extent.label('y'))
+        axes[1].set_title('Tree projection')
+
+        axes[2].imshow(rel_M.value, extent=tree_projector.extent.value)
+        axes[2].set_ylabel(tree_projector.extent.label('y'))
+        axes[2].set_xlabel(tree_projector.extent.label('x'))
+        axes[2].set_title('Rel difference')
 
 plt.show()

paicos/image_creators/tree_projector.py
@@ -303,7 +303,14 @@ class TreeProjector(ImageCreator):
             avail_list = (list(self.snap.keys()) + self.snap._auto_list)
             if f'{parttype}_Volume' in avail_list:
                 variable_density = variable[self.index] / self.snap[f'{parttype}_Volume'][self.index]
-                projection = np.sum(variable_density * self.delta_depth, axis=2) / self.depth
+
+                projection = np.sum(variable_density * self.delta_depth, axis=2)
+                """
+                # Note that the above is equivalent to:
+                volume_per_voxel = area_per_pixel * self.delta_depth
+                projection = np.sum(variable_density * volume_per_voxel, axis=2) \
+                             / area_per_pixel
+                """
             else:
                 err_msg = (f"The volume field for parttype {parttype} is required when"
                            + "using additive=True")
@@ -311,5 +318,10 @@ class TreeProjector(ImageCreator):
         else:
             variable = variable[self.index]
             projection = np.sum(variable * self.delta_depth, axis=2) / self.depth
+            """
+            # Note that the above is equivalent to:
+            projection = np.mean(variable, axis=2)
+            since self.delta_depth is a constant.
+            """
 
         return projection

tests/non-comoving/test_tree_projector_vs_sph_projector.py
@@ -1,0 +1,84 @@
+
+def test_tree_projector_vs_sph_projector(show=False):
+    import paicos as pa
+    import numpy as np
+
+    snap = pa.Snapshot(pa.data_dir, 7,
+                       basename='small_non_comoving')
+
+    widths = snap.box_size.copy
+    center = snap.box_size.copy / 2
+
+    tree_projector = pa.TreeProjector(snap, center, widths, 'z', npix=300)
+    projector = pa.Projector(snap, center, widths, 'z', npix=300, nvol=24)
+
+    tree_M = tree_projector.project_variable('0_Masses')
+    tree_V = tree_projector.project_variable('0_Volume')
+    tree_rho = tree_M / tree_V
+    #    rho = tree_projector.project_variable('0_Density', extrinsic=False)
+
+    M = projector.project_variable('0_Masses')
+    V = projector.project_variable('0_Volume')
+    rho = M / V
+
+    rel_rho = (np.abs(tree_rho - rho) / tree_rho)
+    rel_M = (np.abs(tree_M - M) / tree_M)
+
+    assert np.max(rel_rho.value) < 0.17, np.max(rel_rho.value)
+
+    # Only check away from boundaries since we do not do these correctly
+    # with the SPH projector.
+    M_err = np.max(rel_M[35:65, 100:200])
+    assert M_err < 0.16, M_err
+
+    if show:
+        import matplotlib.pyplot as plt
+        for ii in range(1, 3):
+
+            plt.figure(ii)
+            plt.clf()
+            fig, axes = plt.subplots(num=ii, nrows=3, sharex=True)
+
+            if ii == 1:
+                # Define new imshow_wo_units that automatically plots the values
+                fig.suptitle('Density plots (by dividing additive projections)')
+                imshow_wo_units = pa.util.remove_astro_units(axes[0].imshow)
+                imshow_wo_units(rho, extent=projector.extent,
+                                vmin=rho.min(), vmax=rho.max())
+                axes[0].set_ylabel(projector.extent.label('y'))
+                axes[0].set_title('Sph projection')
+
+                imshow_wo_units = pa.util.remove_astro_units(axes[1].imshow)
+                imshow_wo_units(tree_rho.value, extent=tree_projector.extent.value,
+                                vmin=rho.min(), vmax=rho.max())
+                axes[1].set_ylabel(tree_projector.extent.label('y'))
+                axes[1].set_title('Tree projection')
+
+                axes[2].imshow(rel_rho.value, extent=tree_projector.extent.value)
+                axes[2].set_ylabel(tree_projector.extent.label('y'))
+                axes[2].set_xlabel(tree_projector.extent.label('x'))
+                axes[2].set_title('Rel difference')
+
+            if ii == 2:
+                fig.suptitle('Mass plots')
+                # Define new imshow_wo_units that automatically plots the values
+                imshow_wo_units = pa.util.remove_astro_units(axes[0].imshow)
+                imshow_wo_units(M, extent=projector.extent)
+                axes[0].set_ylabel(projector.extent.label('y'))
+                axes[0].set_title('Sph projection')
+
+                imshow_wo_units = pa.util.remove_astro_units(axes[1].imshow)
+                imshow_wo_units(tree_M.value, extent=tree_projector.extent.value)
+                axes[1].set_ylabel(tree_projector.extent.label('y'))
+                axes[1].set_title('Tree projection')
+
+                axes[2].imshow(rel_M.value, extent=tree_projector.extent.value)
+                axes[2].set_ylabel(tree_projector.extent.label('y'))
+                axes[2].set_xlabel(tree_projector.extent.label('x'))
+                axes[2].set_title('Rel difference')
+
+        plt.show()
+
+
+if __name__ == '__main__':
+    test_tree_projector_vs_sph_projector(True)