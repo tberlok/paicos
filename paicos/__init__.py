dfd1ff47804e3e2a31e7d2ba6b2508179d8c05a3
Thomas Berlok <tberlok@gmail.com>
2024-04-10 14:31:25 +0200

    small p in paicos in .toml ...

--- paicos/__init__.py
+++ paicos/__init__.py
@@ -8,7 +8,7 @@ The code is parallel with an OpenMP Cython implementation
 and a CUDA GPU implementation for visualization.
 """
 
-__version__ = "0.1.11"
+__version__ = "0.1.12"
 __author__ = 'Thomas Berlok'
 __credits__ = 'Niels Bohr Institute, University of Copenhagen'
 
