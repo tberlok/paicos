aafb0a619f17fb32b06b3abb90ec534ecd4ad64b
Thomas Berlok <tberlok@gmail.com>
2024-04-10 10:40:49 +0200

    Bump version number

--- paicos/__init__.py
+++ paicos/__init__.py
@@ -8,7 +8,7 @@ The code is parallel with an OpenMP Cython implementation
 and a CUDA GPU implementation for visualization.
 """
 
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 __author__ = 'Thomas Berlok'
 __credits__ = 'Niels Bohr Institute, University of Copenhagen'
 
