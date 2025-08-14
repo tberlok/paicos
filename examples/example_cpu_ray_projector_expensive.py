8622ae986f8805b90f41a4d66fc3164a277fdf27
Thomas Berlok <tberlok@gmail.com>
2025-08-14 21:25:37 +0200

    Illustrate regime in which RayProjector is faster

--- examples/example_cpu_ray_projector_expensive.py
+++ examples/example_cpu_ray_projector_expensive.py
@@ -1,4 +1,3 @@
-
 import paicos as pa
 import numpy as np
 pa.use_units(False)
@@ -7,32 +6,12 @@ pa.numthreads(pa.settings.max_threads)
 import matplotlib.pyplot as plt
 from matplotlib.colors import LogNorm
 
-snap = pa.Snapshot(pa.data_dir, 247, basename='snap',
-                   load_catalog=True)
-
-# snap['0_Coordinates'] = snap['0_Coordinates'][:9]
-# snap['0_Volume'] = snap['0_Volume'][:9]
-# snap['0_Masses'] = snap['0_Masses'][:9]
-# snap['0_Density'] = snap['0_Density'][:9]
-
-# for jj in range(3):
-#     for kk in range(3):
-#         ii = jj * 3 + kk
-#         snap['0_Volume'][ii] = 4 * np.pi / 3 * (snap.box / 2 / (10 * (ii + 1)))**3
-#         snap['0_Coordinates'][ii][0] = snap.box * 1 / 4
-#         snap['0_Coordinates'][ii][1] = snap.box * 1 / 4
-#         snap['0_Coordinates'][ii][2] = snap.box * 1 / 2
-#         snap['0_Masses'][ii] = snap['0_Volume'][ii] * snap['0_Density'][ii]
-#         # snap['0_Density'][ii] = 1.0
-#         snap['0_Coordinates'][ii][0] += jj * snap.box * 1 / 4
-#         snap['0_Coordinates'][ii][1] += kk * snap.box * 1 / 4
-
+snap = pa.Snapshot(pa.data_dir, 247, basename='snap', load_catalog=True)
 
 plt.rc('image', origin='lower', cmap='RdBu_r', interpolation='None')
 plt.figure(1)
 plt.clf()
-fig, axes = plt.subplots(num=1, ncols=2,
-                         sharex='col', sharey='col')  # , sharey=True)
+fig, axes = plt.subplots(num=1, ncols=3, sharex='col', sharey='col')
 
 widths = [snap.box/2, snap.box/2, snap.box/2]
 center = [snap.box / 2, snap.box / 2, snap.box / 2]
@@ -40,23 +19,20 @@ center = [snap.box / 2, snap.box / 2, snap.box / 2]
 widths = [20000, 20000, 20000]
 center = snap.Cat.Group['GroupPos'][0]
 
-for ii, direction in enumerate(['z']):
+r = pa.RayProjector(snap, center, widths, 'z', npix=128, tol=0.05, verbose=False, timing=True, do_pre_selection=True)
 
-    r = pa.RayProjector(snap, center, widths, direction, npix=512, tol=0.25, verbose=False, timing=True, do_pre_selection=True)
+density = r.project_variable('0_Density', additive=False, timing=True)
 
-    density = r.project_variable('0_Density', additive=False, timing=True)
+t = pa.TreeProjector(snap, center, widths, 'z', npix=128, tol=0.05, verbose=False, timing=True)
 
+t_density = t.project_variable('0_Density', additive=False, timing=True)
 
-    t = pa.TreeProjector(snap, center, widths, direction, npix=512, tol=1, verbose=False, timing=True)
+# Make a plot
+axes[0].imshow(t_density, origin='lower', extent=t.extent, norm=LogNorm())
+axes[1].imshow(density, origin='lower', extent=r.extent, norm=LogNorm())
+axes[2].imshow((density - t_density) / density, origin='lower', extent=r.extent)
 
-    t_density = t.project_variable('0_Density', additive=False, timing=True)
-
-    # Make a plot
-    axes[0].imshow(t_density, origin='lower', extent=t.extent, norm=LogNorm())
-    axes[1].imshow(density, origin='lower', extent=r.extent, norm=LogNorm())
-    # print(np.max(np.abs(normal_image-nested_image)/normal_image))
-    # print(np.sum(normal_image.flatten()), np.sum(nested_image.flatten()))
-
-    axes[0].set_title('TreeProjector')
-    axes[1].set_title('RayProjector')
-    plt.show()
+axes[0].set_title('TreeProjector')
+axes[1].set_title('RayProjector')
+axes[2].set_title('Rel. difference')
+plt.show()
